9a922dd5dd7680478440826bac2eb546615abf21
Annie Cheng <ac962@cornell.edu>
2015-11-12 20:34:40 -0500

    Add player mli file

---
+++ player.mli
@@ -0,0 +1,18 @@
+open Feunit
+open Terrain
+
+(*
+ * The Player module
+ *   - Takes in keyboard input, FEUnits, and Game State Manager
+ *   - Applies change to the Game State
+ *   - Controls the player units and draws the cursor
+ *)
+
+(* Returns a new game state by moving a unit *)
+val update : unit -> unit list list -> terrain list list ->
+                (unit list list, terrain list list)
+
+(* finds player units to move*)
+val find_player_units: unit list list -> unit list
+
+
