4c3da7df05d8e18fdfa0881e6094956364776057
Houka <cl795@cornell.edu>
2015-11-12 21:08:21 -0500

    updated ai and level

--- ai.mli
+++ ai.mli
@@ -4,6 +4,9 @@ open Terrain
  * The AI module
  *   - Sets commands for the units
  *   - Updates the units based on algorithms
+ *        - Rush Nearest
+ *        - Swarm Lowest Health
+ *        - Focus On Easiest Unit To Take Down
  *)
 
 (* Returns a new game state by moving a unit *)
--- level.mli
+++ level.mli
@@ -6,7 +6,8 @@ open Terrain
  *)
 
 (*stores locations and status of each unit and terrain*)
-type level = {unitMatix: feunit list list; terrainMatrix: terrain list list}
+type level = {name: string; unitMatix: feunit list list;
+              terrainMatrix: terrain list list}
 
 (*returns the corresponding level based on string input*)
 val getLevel: string -> level
