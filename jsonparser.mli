a2841176edec7685afcfeed60b3c7798367e35b1
Houka <cl795@cornell.edu>
2015-11-12 20:50:42 -0500

    updates to some mli to conform to style

--- jsonparser.mli
+++ jsonparser.mli
@@ -1,23 +1,21 @@
+open Feunit
+open Terrian
+open Level
+open Yojson.Basic.Util
 (*
  * The Json Parser module
  *   - Sets the initial state of the game from a json file.
  *)
-open Yojson.Basic.Util
-
-type unit = {attack: int; defense: int; move: int; max_hp: int;
-                mutable current_hp: int; weapon: string; image:string}
-type terrain = {atk_bonus:int; def_bonus: int; image: string}
 
 (* Returns the matrix of unit locations *)
-val get_unit_locs : Yojson.Basic.json -> unit list list
+val getUnitMatrix : unit -> int list list
 
 (* Returns the matrix of terrain locations *)
-val get_terrain_locs : Yojson.Basic.json -> terrain list list
+val getTerrainMatrix : unit -> int list list
 
-(* Returns the number of allies given the unit location list*)
-val num_allies : unit list list -> int
-
-(* Returns the number of enemies given the unit location list*)
-val num_enemies : unit list list -> int
-
-
+(* Getter funtions that parses the json file into their repective forms and
+ *  returns a list of all the items
+  *)
+val getAllUnitData : unit -> feunit list
+val getAllTerrainData : unit -> terrain list
+val getAllLevelData : unit -> level list
--- level.mli
+++ level.mli
@@ -1,15 +1,12 @@
+open Feunit
+open Terrain
 (*
  * The Level module
  *   - Stores the state of the game
  *)
 
-open Yojson.Basic.Util
-open Feunit
-open Terrain
-
-
 (*stores locations and status of each unit and terrain*)
-type level = {unit_locs: feunit list list; terrain_locs: terrain list list}
+type level = {unitMatix: feunit list list; terrainMatrix: terrain list list}
 
 (*returns the corresponding level based on string input*)
-val get_level: string -> level
+val getLevel: string -> level
