5660a2c9770a7057ac801a9c7f25a46140ad4a25
alexxu1 <ax35@cornell.edu>
2015-11-12 21:12:37 -0500

    fixed typo

--- level.mli
+++ level.mli
@@ -6,7 +6,7 @@ open Terrain
  *)
 
 (*stores locations and status of each unit and terrain*)
-type level = {unitMatix: feunit list list; terrainMatrix: terrain list list}
+type level = {unitMatrix: feunit list list; terrainMatrix: terrain list list}
 
 (*returns the corresponding level based on string input*)
 val getLevel: string -> level
