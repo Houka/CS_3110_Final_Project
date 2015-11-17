3d8c3e1c641c583870041c36a0f706a0d88d3de1
alexxu1 <ax35@cornell.edu>
2015-11-17 17:25:44 -0500

    finished jsonparser and tested

--- jsonparser.ml
+++ jsonparser.ml
@@ -12,14 +12,96 @@ let json = Yojson.Basic.from_file "example.json"
 type terrain_info = {name:string; atkBonus:int; defBonus: int;
                       terrain_type:string; img: string}
 
-type feunit_info = { name: string; maxHp: int; actions: action list;
-              atk: int; def: int; atkRange: int; movRange: int;
-              hp: int; weapon: string; img: string }
-let get_all_unit_data ()
-  = failwith "TODO"
+type feunit_info = { name: string; maxHp: int; atk: int; def: int;
+                    movRange: int; atkRange: int; weapon: string; img: string }
 
 
-(*below are the functions used for getting terrain data*)
+(*in this section the functions are used for getting unit data*)
+
+let extract_unit () =
+  [json]
+  |> filter_member "units"
+  |> flatten
+
+let extract_unit_name ():string list =
+  extract_unit()
+  |> filter_member "name"
+  |> filter_string
+
+let extract_unit_class ():int list  =
+  extract_unit()
+  |> filter_member "class"
+  |> filter_int
+
+let extract_unit_maxhp () :int list =
+  extract_unit()
+  |> filter_member "maxhp"
+  |> filter_int
+
+let extract_unit_attack (): int list =
+  extract_unit()
+  |> filter_member "attack"
+  |> filter_int
+
+let extract_unit_defense (): int list =
+  extract_unit()
+  |> filter_member "defense"
+  |> filter_int
+
+let extract_unit_move (): int list =
+  extract_unit()
+  |> filter_member "move"
+  |> filter_int
+
+let extract_unit_range (): int list =
+  extract_unit()
+  |> filter_member "range"
+  |> filter_int
+
+let extract_unit_weapon (): string list =
+  extract_unit()
+  |> filter_member "weapon"
+  |> filter_string
+
+let extract_unit_image ():string list =
+  extract_unit()
+  |> filter_member "image"
+  |> filter_string
+
+
+let get_all_unit_data (): (int*feunit_info) list =
+  let names = extract_unit_name () in
+  let classes = extract_unit_class () in
+  let maxhp = extract_unit_maxhp () in
+  let attack = extract_unit_attack () in
+  let defense = extract_unit_defense () in
+  let move = extract_unit_move () in
+  let ranges = extract_unit_range () in
+  let weapons = extract_unit_weapon () in
+  let images = extract_unit_image () in
+
+  let rec make_list a b c d e f g h i: (int*feunit_info) list=
+  match a,b,c,d,e,f,g,h,i with
+    | [], [], [], [], [], [], [], [], [] -> []
+    | h1::t1, h2::t2, h3::t3, h4::t4, h5::t5, h6::t6, h7::t7, h8::t8, h9::t9->
+      (h2,{name = h1; maxHp = h3; atk = h4; def= h5; movRange = h6;
+        atkRange= h7; weapon = h8; img = h9})
+              :: make_list t1 t2 t3 t4 t5 t6 t7 t8 t9
+    | _ -> failwith "Invalid json"
+  in make_list names classes maxhp attack defense move ranges weapons images
+
+TEST_UNIT = let data = get_all_unit_data () in
+            assert (data =
+              [(0, {name = "null"; maxHp = 9999; atk = 0 ;
+              def = 0; movRange = 1; atkRange = 0; weapon = "null";
+              img = "null.png"});
+            (1, {name = "swordsman"; maxHp = 100; atk = 5 ;
+              def = 10; movRange = 3; atkRange = 1; weapon = "sword";
+              img = "swordsman.png"})] )
+
+
+
+(*in this section are the functions used for getting terrain data*)
 let extract_terrain () =
   [json]
   |> filter_member "terrain"
@@ -72,7 +154,7 @@ let get_all_terrain_data () : (int*terrain_info) list=
   in make_list names classes atkBonus defBonus images types
 
 (*test getting terrain from examples.json*)
-let _ = let data = get_all_terrain_data () in
+TEST_UNIT = let data = get_all_terrain_data () in
         assert (data = [(1, {name = "grass"; atkBonus = 1; defBonus = 1;
                             img = "grass.jpg"; terrain_type = "plain"})])
 
