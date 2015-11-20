96793a1a8d5dc62e11313645a090cdac78153e9a
Houka <cl795@cornell.edu>
2015-11-19 23:43:13 -0500

    Sprite completed

--- sprite.ml
+++ sprite.ml
@@ -1,10 +1,34 @@
+type image = OImages.oimage
 
-let get_image (filename: string) : Images.t = failwith "TODO"
+let load_image (filename:string) : image =
+  let img = OImages.load filename [] in
+  match OImages.tag img with
+  |  OImages.Rgb24 x -> img
+  | OImages.Rgba32 x -> let img = x#to_rgb24 in img#coerce
+  | _ -> failwith "unsupported format"
 
-let resize (img: Images.t) (width: int) (height: int) : Images.t =
-  failwith "TODO"
+(* loads all images into a mapping *)
+let images =
+  let imgs = Jsonparser.get_images () in
+  List.fold_left (fun a x -> (x,load_image x) :: a) [] imgs
 
-let scale (img: Images.t) (s: int) : Images.t = failwith "TODO"
 
-let crop (img: Images.t) (x,y) (width: int) (height: int) : Images.t =
-  failwith "TODO"
+let get_image (filename: string) : image =
+  List.assoc filename images
+
+let resize (img: image) (width: int) (height: int) : image =
+  let img = OImages.rgb24 img in
+  let img = img#resize None width height in
+  img#coerce
+
+let scale (img: image) (s: float) : image =
+  let width = float_of_int (img#width) in
+  let height = float_of_int (img#height) in
+  resize img (int_of_float(width *. s)) (int_of_float(height *. s))
+
+let crop (img:image) (x,y) (height: int) (width: int) : image =
+  OImages.sub img x y height width
+
+let draw (img: image) (x,y) : unit =
+  let img = img#image in
+  Graphic_image.draw_image img x y;;
--- sprite.mli
+++ sprite.mli
@@ -2,25 +2,33 @@
  * The Sprite Module.
  *   - Loads all image data from json on startup. Then stores it in a drawable
  *      format that Graphics can use to put in the GUI
- *   - Images stored in a (string*Images.t) list ref which is accessed to get img
+ *   - Images stored in a (string*image) list ref which is accessed to get img
  *)
+
+type image
 
 (* [getImage filename] returns the image associated with the file named
  *  [filename].
  *  Precondition: [filename] points to a .png file
  *)
-val get_image: string -> Images.t
+val get_image: string -> image
 
 (* [resize img w h] returns an altered image of [img] that has width [w] and
  *   height [h]
 *)
-val resize: Images.t -> int -> int -> Images.t
+val resize: image -> int -> int -> image
 
 (* [scale img x] is the image [img] scaled to value x *)
-val scale: Images.t -> int -> Images.t
+val scale: image -> float -> image
 
 (* [crop img (x,y) w h] returns an portion of [img] where ([x], [y]) is the
  *  starting coordinate relative to the bottom-left corner of [img] and
  *  [w] and [h] is the new image size. Its the basic definition of cropping.
  *)
-val crop: Images.t -> (int*int) -> int -> int -> Images.t
+val crop: image -> (int*int) -> int -> int -> image
+
+(* [draw img (x,y)] Draws the image onto the currently open GUI at coordinate (x,y).
+ * Note: (0,0) is at the bottom left of the gui window
+ * Prec : must have a Graphics gui open
+ *)
+val draw: image -> (int*int) -> unit
