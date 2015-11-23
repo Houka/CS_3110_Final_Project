88577f80090cff61f86579c94f84d5964e9daf2b
leotrj <lt342@cornell.edu>
2015-11-22 22:25:30 -0500

    constants changed

--- constants.ml
+++ constants.ml
@@ -1,7 +1,12 @@
 open Feunit
 
 (* Alloted actions that each Player can take *)
-type action = Wait of feunit | Move of feunit * int * int | Attack of feunit * feunit
+type action = Wait of feunit * (int * int)
+              | Move of feunit * (int * int) * (int * int)
+              | Attack of feunit * (int * int) * feunit * (int * int)
 
+(* 2 Dimensional Array type*)
+type 'a matrix = 'a list list
 (* The side length of each square in the game grid *)
 let gridSide = 50;
+
