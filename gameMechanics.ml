19ec6a47b35256edbd150362e7a398d827db55c8
alexxu1 <ax35@cornell.edu>
2015-11-25 12:05:06 -0500

    fixed a bug in gameMechanics where turn kept incrementing by two. Somehow this got fixed by me adding line 257. If this is removed program fails when it runs by repeatedly drawing over and over and not taking user input.

--- gameMechanics.ml
+++ gameMechanics.ml
@@ -8,6 +8,10 @@ let turn = ref 0
 let currentUnits = ref [||]
 let currentTerrains = ref [||]
 
+let num_allies = ref 0
+let num_enemies = ref 0
+let num_usable_units = ref 1
+
 (* converts an 'a array array to 'a matrix *)
 let to_2d_list (matrix: 'a array array) : 'a matrix =
   let first_layer = Array.to_list matrix in
@@ -40,10 +44,17 @@ let opposite_sides (a:feunit) (b:feunit) =
   | Enemy _, Ally _ -> true
   | _ -> false
 
+let type_of (u:feunit):string =
+  match u with
+  |Ally _ -> "Ally"
+  |Enemy _ -> "Enemy"
+  |Null -> "Null"
 (* legal actions *)
 let attack_unit (x1,y1) (x2,y2): unit =
   let unit1 = get_unit (x1,y1) in
   let unit2 = get_unit (x2,y2) in
+  let unit1_type = type_of unit1 in
+  let unit2_type = type_of unit2 in
   if not (opposite_sides unit1 unit2) then failwith "units are allied" else
   if get_endturn unit1 then failwith "unit cannot attack, turn is over" else
   if (not (in_range (x1,y1) (x2, y2) (get_total_range unit1)))
@@ -52,14 +63,19 @@ let attack_unit (x1,y1) (x2,y2): unit =
 
     let (a,b) = attack unit1 unit2 in
     let () =
-    if b = Null then !currentUnits.(y2).(x2) <- Null
+    if b = Null then (!currentUnits.(y2).(x2) <- Null;
+      (if unit2_type = "Ally" then num_allies := !num_allies - 1
+                             else num_enemies := !num_enemies - 1))
     else
     (*counterattack*)
       if (in_range (x2,y2) (x1, y1) (get_total_range unit2)) then
         let (c,d) = attack b a in
-        if d = Null then !currentUnits.(y1).(x1) <- Null else ()
+        if d = Null then (!currentUnits.(y1).(x1) <- Null;
+                    (if unit1_type = "Ally" then num_allies := !num_allies - 1
+                                            else num_enemies := !num_enemies - 1))
+        else ()
       else ()
-    in set_endturn unit1 true
+    in set_endturn unit1 true(* ;num_usable_units := !num_usable_units-1 *)
 
 
 let move (x1,y1) (x2,y2) : unit =
@@ -76,7 +92,8 @@ let move (x1,y1) (x2,y2) : unit =
 let wait (x,y) : unit =
   let u = get_unit (x,y) in
   let endturn = get_endturn u in
-  if endturn then failwith "This unit's turn is over" else set_endturn u false
+  if endturn then failwith "This unit's turn is over"
+             else set_endturn u false(* ; num_usable_units := !num_usable_units-1 *)
 
 let end_turns (): unit =
   let switch (a:feunit) :unit =
@@ -84,7 +101,8 @@ let end_turns (): unit =
       | Ally _ -> set_endturn a true; set_hasMoved a true
       | Enemy _ -> set_endturn a true; set_hasMoved a true
       | _ -> () in
-  Array.iter (fun a -> Array.iter (switch) a) !currentUnits
+  (Array.iter (fun a -> Array.iter (switch) a) !currentUnits)(* ;
+  num_usable_units := 0 *)
   (* let switch s (a:feunit) :unit =
   match s with
   | "Ally" -> let () =
@@ -99,13 +117,58 @@ let end_turns (): unit =
   | _ -> () in
   Array.iter (fun a -> Array.iter (switch s) a) !currentUnits *)
 
+(*count allies*)
+let count_allies (): int =
+  let count = ref 0 in
+  let () = for y = 0 to (Array.length !currentUnits)-1 do
+                for x = 0 to (Array.length !currentUnits.(0))-1 do
+                  match (!currentUnits.(y).(x)) with
+                  | Ally _ -> incr count
+                  | _ -> ()
+                done
+            done in
+  !count
+
+(*count enemies*)
+let count_enemies (): int =
+  let count = ref 0 in
+  let () = for y = 0 to (Array.length !currentUnits)-1 do
+                for x = 0 to (Array.length !currentUnits.(0))-1 do
+                  match (!currentUnits.(y).(x)) with
+                  | Enemy _ -> incr count
+                  | _ -> ()
+                done
+            done in
+  !count
+
+(*starts start_turns for "Ally" or "Enemy"*)
+let start_turns s :unit =
+  let switch s (a:feunit) :unit =
+  match s with
+  | "Ally" -> let () =
+              match a with
+              | Ally _ -> set_endturn a false; set_hasMoved a false
+              | _ -> ()
+              in ()
+  | "Enemy" -> let () = match a with
+              | Enemy _ -> set_endturn a false; set_hasMoved a false
+              | _ -> ()
+              in ()
+  | _ -> () in
+  Array.iter (fun a -> Array.iter (switch s) a) !currentUnits
+
+
 let set_units (feunits:feunit array array) : unit =
   currentUnits := feunits;
-  turn := 0
+  turn := 1;
+  num_allies := count_allies ();
+  num_enemies := count_enemies ();
+  num_usable_units := !num_allies;
+  start_turns "Ally"
 
 let set_map (map:terrain array array) : unit =
   currentTerrains := map;
-  turn := 0
+  turn := 1
 
 
 
@@ -172,61 +235,27 @@ let draw () : unit =
   Hub.draw_unit_stats highlightedUnit;
   Hub.draw_terrain_stats highlightedTerrain *)
 
-(*starts start_turns for "Ally" or "Enemy"*)
-let start_turns s :unit =
-  let switch s (a:feunit) :unit =
-  match s with
-  | "Ally" -> let () =
-              match a with
-              | Ally _ -> set_endturn a false; set_hasMoved a false
-              | _ -> ()
-              in ()
-  | "Enemy" -> let () = match a with
-              | Enemy _ -> set_endturn a false; set_hasMoved a false
-              | _ -> ()
-              in ()
-  | _ -> () in
-  Array.iter (fun a -> Array.iter (switch s) a) !currentUnits
 
-(*count allies*)
-let count_allies (): int =
-  let count = ref 0 in
-  let () = for y = 0 to (Array.length !currentUnits)-1 do
-                for x = 0 to (Array.length !currentUnits.(0))-1 do
-                  match (!currentUnits.(y).(x)) with
-                  | Ally _ -> incr count
-                  | _ -> ()
-                done
-            done in
-  !count
 
-(*count enemies*)
-let count_enemies (): int =
-  let count = ref 0 in
-  let () = for y = 0 to (Array.length !currentUnits)-1 do
-                for x = 0 to (Array.length !currentUnits.(0))-1 do
-                  match (!currentUnits.(y).(x)) with
-                  | Enemy _ -> incr count
-                  | _ -> ()
-                done
-            done in
-  !count
 
 let rec update () : unit =
-  if count_allies () = 0 then print_string "Enemies win.\n" else
-  if count_enemies () = 0 then print_string "You win!\n" else
-  (*if turn is even it is Player's turn; if it is odd it is enemy turn*)
+  if !num_allies = 0 then print_string "Enemies win.\n" else
+  if !num_enemies = 0 then print_string "You win!\n" else
+  (*if turn is odd it is Player's turn; if it is even it is enemy turn*)
   if !turn mod 2 = 1
   then
-      if not (turn_over "Ally")
+      (* let () = print_int !turn in *)
+      if not (!num_usable_units = 0)
       then
         let actions = Player.update (get_units ()) (get_map ()) in
         perform_actions actions
       else
         (start_turns "Enemy";
-        inc_turn ())
+        inc_turn ();
+        num_usable_units := !num_enemies)
   else
-      if not (turn_over "Enemy")
+      let () = print_int !turn in
+      if not (!num_usable_units = 0)
       then
         let actions = Ai.update (get_units ()) (get_map ()) in
         perform_actions actions;
@@ -234,4 +263,5 @@ let rec update () : unit =
         update()
       else
         start_turns "Ally";
-        inc_turn ()
+        inc_turn ();
+        num_usable_units := !num_enemies
